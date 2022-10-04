0908f72523aea97aee315b520bce56b2bc68f780
John Roller <jtroller33@gmail.com>
2022-10-03 21:35:33 -0500

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');         // ????
+// const helpers = require('./utils/helpers');         // ????
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });     // ????
+// const hbs = exphbs.create({ helpers });     // ????
 
 const sess = {
   secret: 'Super secret secret',
@@ -30,8 +30,8 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
